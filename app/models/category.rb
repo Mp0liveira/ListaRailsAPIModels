3c3162d39560aebceb011e4ceb44c0c5140dec7c
marcos <marcosp.oliveir4@gmail.com>
2023-06-04 17:40:19 -0300

    success - validating description max characters

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
     validates :name, presence: :true, uniqueness: :true
-    validates :description, presence: :true, uniqueness: :true
+    validates :description, presence: :true, uniqueness: :true, length: {maximum: 10000}
 end
