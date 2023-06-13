b9928065f06fb84fd84c1fc6a398871c4c6d92be
marcos <marcosp.oliveir4@gmail.com>
2023-06-13 19:29:32 -0300

    add PostCategory and Post relation

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,3 +1,4 @@
 class PostCategory < ApplicationRecord
   belongs_to :category
+  belongs_to :post
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe PostCategory, type: :model do
   context "testing factory" do
     it "should create post category" do
       create(:category, id:1)
-      expect(build(:post_category, category_id:1)).to be_valid
+      create(:post, id:1)
+      expect(build(:post_category, category_id: 1, post_id: 1)).to be_valid
     end
   end
 end
