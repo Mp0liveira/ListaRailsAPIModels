898ab0a751aca509f3eb520763c151d4a0f1f182
marcos <marcosp.oliveir4@gmail.com>
2023-06-13 20:13:53 -0300

    success - testing relation between Post and Commentary 2

--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -6,5 +6,8 @@ RSpec.describe Commentary, type: :model do
       create(:post, id: 1)
       expect(create(:commentary, post_id: 1)).to be_valid
     end
+    it "should not be created if post doesn't exist" do
+      expect(build(:commentary)).to be_invalid
+    end
   end
 end
