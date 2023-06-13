0129a989427f5f17353ac675417ca2a49389f550
marcos <marcosp.oliveir4@gmail.com>
2023-06-13 19:08:55 -0300

    failed - Post nil title

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Post, type: :model do
   context "testing factory" do
     it {expect(build(:post)).to be_valid}
   end
+
+  context "testing title" do
+    it "should be invalid if nil" do
+      expect(build(:post, title: nil)).to be_invalid
+    end
+  end
 end
