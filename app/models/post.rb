548896999fe7bc188c4a33ae628234372561fc74
marcos <marcosp.oliveir4@gmail.com>
2023-06-13 19:31:49 -0300

    add PostCategory and Post relation 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    has_many :post_categories, dependent: :destroy
+
     validates :title, presence: :true, length: {maximum: 20}
     validates :content, presence: :true
 end
