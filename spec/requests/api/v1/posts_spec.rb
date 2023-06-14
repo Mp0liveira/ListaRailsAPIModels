15679bb164c22847f2aa8e823674b672cf1c40c9
marcos <marcosp.oliveir4@gmail.com>
2023-06-14 17:09:34 -0300

    failed - Post PATCH params ok

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -27,4 +27,17 @@ RSpec.describe "Api::V1::Posts", type: :request do
       end
     end
   end
+
+  describe "PATCH /update/:id" do
+    let(:post) {create(:post, title: "Fogo", content: "fogao ta embalado")}
+    let(:post_params) do
+      attributes_for(:post)
+    end
+    context "when params are ok" do
+      it "return https status ok" do
+        patch "/api/v1/posts/update/#{post.id}", params:{post: post_params}
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
 end
