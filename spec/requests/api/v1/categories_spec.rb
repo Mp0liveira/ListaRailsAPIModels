02acbf0650ce1e92bbb0b944d32e83fc8971b53b
marcos <marcosp.oliveir4@gmail.com>
2023-06-14 18:15:15 -0300

    success - Category POST when params are repeated

--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -11,9 +11,16 @@ RSpec.describe "Api::V1::Categories", type: :request do
         expect(response).to have_http_status(:created)
       end
     end
-    context "whem params are nil" do
+    context "when params are nil" do
       it "return http status bad request" do
         category_params = nil
+        post "/api/v1/categories/create", params:{category: category_params}
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+    context "when param are repetead" do
+      it "return http status bad request" do
+        post "/api/v1/categories/create", params:{category: category_params}
         post "/api/v1/categories/create", params:{category: category_params}
         expect(response).to have_http_status(:bad_request)
       end
