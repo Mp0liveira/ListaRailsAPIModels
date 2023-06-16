2104abeed3aa020da5a6bf6e04d7b4c6e05ffcab
marcos <marcosp.oliveir4@gmail.com>
2023-06-16 20:11:47 -0300

    povoando a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+categorias = Category.create([{id: 1, name: "Esportes", description: "Encontre tudo sobre esportes"},
+ {id: 2, name: "Política", description: "Acompanhe a política mundial"}, {id: 3, name: "Jogos", description: "Acompanhe sobre o mundo dos jogos"}])
+
+posts = Post.create([{id: 1, title: "Fogo", content: "O líder do Brasileirão volta a campo no próximo sábado, contra o Cuiabá, fora de casa."},
+ {id: 2, title: "Trump é preso", content: "Ex-presidente dos EUA, Trump se entregou a justiça estadunidense até o fim da investigação"},
+ {id: 3, title: "O sucesso imbatível", content: "CS2, o novo jogo da franquia de Counter Strike Global Offensive vem fazendo sucesso na sua versão beta."}])
+
+feedbacks = Feedback.create([{like: true, post_id: 1}, {like: true, post_id: 1}, {like: true, post_id: 2}, {like: false, post_id: 2}, {like: true, post_id: 3},
+ {like: true, post_id: 3}])
+
+comentarios = Commentary.create([{content: "Segue o líder", post_id: 1}, {content: "Cavalo paraguaio, jaja cai", post_id: 1},
+ {content: "Tem que ser preso esse bandido mesmo", post_id: 2}, {content: "Que continue por um bom tempo!", post_id: 2},
+ {content: "CS nunca morrerá", post_id: 3}, {content: "Os gráficos tão f0d4s demais"}, post_id: 3])
