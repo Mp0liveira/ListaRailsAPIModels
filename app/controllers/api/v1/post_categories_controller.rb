8c0b9435e49457a62e5d798710f6944e0f5f820e
marcos <marcosp.oliveir4@gmail.com>
2023-06-16 19:30:39 -0300

    failed - Categories show when exists

--- app/controllers/api/v1/post_categories_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::PostCategoriesController < ApplicationController
-end
--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Categories", type: :request do
+  describe "GET /show" do
+    let!(:category) { create(:category, name: "test", description: "testing") }
+
+    context "when the category exists" do
+      it "returns the category details" do
+        get "/api/v1/categories/show/#{category.id}"
+        expect(response).to have_http_status(:ok)
+
+        category_response = JSON.parse(response.body)
+        expect(category_response["name"]).to eq("test")
+        expect(category_response["description"]).to eq("testing")
+      end
+    end
+
+    context "when the category does not exist" do
+      it "returns a not found error" do
+        get "/api/v1/categories/show/999"
+        expect(response).to have_http_status(:not_found)
+
+        error_response = JSON.parse(response.body)
+        expect(error_response["error"]).to eq("couldn't find Category")
+      end
+    end
+  end
+
   describe "POST /create" do
     let(:category_params) do
       attributes_for(:category)
