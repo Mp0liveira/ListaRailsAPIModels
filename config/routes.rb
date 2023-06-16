effcb19fd7101b5c931102b936039cccb6069dd8
marcos <marcosp.oliveir4@gmail.com>
2023-06-16 18:52:46 -0300

    failed - Post PostCategory when params are ok

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,17 @@ Rails.application.routes.draw do
         post ':id/commentaries/create', to: 'commentaries#create'
         patch ':id/commentaries/update/:id', to: 'commentaries#update'
         delete ':id/commentaries/delete/:id', to: 'commentaries#delete'
+        post ':id/post_categories/create', to: 'post_categories#create'
+        patch ':id/post_categories/update/:id', to: 'post_categories#update'
+        delete ':id/post_categories/delete/:id', to: 'post_categories#delete'
       end
       scope 'categories' do
         post 'create', to:'categories#create'
         patch 'update/:id', to:'categories#update'
         delete 'delete/:id', to:'categories#delete'
+        post ':id/post_categories/create', to: 'post_categories#create'
+        patch ':id/post_categories/update/:id', to: 'post_categories#update'
+        delete ':id/post_categories/delete/:id', to: 'post_categories#delete'
       end
     end
   end  
--- spec/requests/api/v1/post_categories_spec.rb
+++ spec/requests/api/v1/post_categories_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::PostCategories", type: :request do
-  describe "GET /index" do
+  describe "POST /create" do
+    let(:post1) {create(:post, title: "Fogo", content: "fogao ta embalado")}
+    let(:category1) {create(:category, name: "Esportes", description: "Tudo sobre esportes")}
+    context "when params are ok" do
+      it "return http status created" do
+        post "/api/v1/posts/#{post1.id}/post_categories/create", params:{category_id: category1.id}
+        expect(response).to have_http_status(:created)
+      end
+    end
   end
 end
